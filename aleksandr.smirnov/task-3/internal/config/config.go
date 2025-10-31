2624e1dff8d2fd648d37004c16ae500a9e158306
A1exCRE <alsmmm2065@gmail.com>
2025-10-31 23:11:02 +0300

    [TASK-3] Add config

---
+++ aleksandr.smirnov/task-3/internal/config/config.go
@@ -0,0 +1,27 @@
+package config
+
+import (
+	"fmt"
+	"os"
+
+	"gopkg.in/yaml.v3"
+)
+
+type Config struct {
+	InputFile  string `yaml:"input-file"`
+	OutputFile string `yaml:"output-file"`
+}
+
+func LoadConfig(path string) (*Config, error) {
+	fileData, err := os.ReadFile(path)
+	if err != nil {
+		return nil, fmt.Errorf("read config file: %w", err)
+	}
+
+	var cfg Config
+	if err := yaml.Unmarshal(fileData, &cfg); err != nil {
+		return nil, fmt.Errorf("parse YAML config: %w", err)
+	}
+
+	return &cfg, nil
+}
